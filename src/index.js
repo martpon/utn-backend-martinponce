1529f4f075929001e7a4917099e6e71c645192df
Martin Exequiel Ponce <martpon17@gmail.com>
2023-12-16 16:20:02 -0300

    inicializando servidor y creando el codigo para tomar peticiones GET

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-console.log("pepe")
+import http from "node:http";
+import dotenv from "dotenv";
+import {readDatabase} from "./utils/utils.js";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const serverHttp = http.createServer((request, response) => {
+
+    const productsParsed = readDatabase();
+
+    switch (request.method) {
+
+        case "GET":
+            if (request.url === "/") {
+
+                const responseServer = {
+                    "status": 200,
+                    "app": "http-server-utn",
+                    "routes": {
+                        "index": "/",
+                        "getProducts": "/products",
+                        "addProducts": "/products"
+                    }
+                };
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+
+                response.end(JSON.stringify(responseServer));
+
+            } else if (request.url === "/products") {
+
+                const responseServer = readDatabase();
+
+                response.writeHead(200,{"Content-type": "application/json" });
+
+                response.end(JSON.stringify(responseServer));
+
+            };
+            break;
+        default:
+
+
+    }
+
+    
+
+})
+
+serverHttp.listen(port, () => {
+        console.log(`server on port: ${port}`);
+
+    })
---
+++ src/utils/utils.js
@@ -0,0 +1,12 @@
+import fs from "node:fs"
+
+const readDatabase = () => {
+
+    const productsBuffer = fs.readFileSync("src/database/products.json")
+    const productsParsed = JSON.parse(productsBuffer.toString());
+
+    return (productsParsed);
+
+}
+
+export {readDatabase}
